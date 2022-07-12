f5fad760224068b9fcd5a201a20d8ddc8586ef81
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-12 16:13:42 +0700

    learn part 3 - add timestamp attend and out

--- app/src/main/java/com/learnkotlin/attendmyparty/MainActivity.kt
+++ app/src/main/java/com/learnkotlin/attendmyparty/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -12,21 +14,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { //this is one of lifecycle, first render
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         member_name.text = "..."
+
         btn_join.setOnClickListener{
+            var timeNow = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
             val randomize = java.util.Random()
             val randomPartyMate = randomize.nextInt(partyMate.count())
             memberName = input_member_name.text.toString()
             member_name.text = "Welcome ${input_member_name.text} !"
+            time_attend.text = "You came at ${timeNow}"
             mate_name.text = "Well, you'll take time with ${partyMate[randomPartyMate]}"
             input_member_name.text.clear()
         }
 
         btn_leave.setOnClickListener {
-            Toast.makeText(applicationContext, "Bye ${memberName}", Toast.LENGTH_SHORT).show()
+            var timeNow = SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+            Toast.makeText(applicationContext, "Bye ${memberName}, you left at ${timeNow}", Toast.LENGTH_SHORT).show()
             member_name.text = "..."
             memberName = ""
+            time_attend.text = ""
             mate_name.text = ""
         }
     }
