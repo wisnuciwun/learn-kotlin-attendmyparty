84b16ef1ae6a77df5f35526953af10f2dcb7d6d3
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-12 11:46:58 +0700

    learn part 1 - welcome page layout & color

--- app/src/main/java/com/learnkotlin/attendmyparty/MainActivity.kt
+++ app/src/main/java/com/learnkotlin/attendmyparty/MainActivity.kt
@@ -2,10 +2,13 @@ package com.learnkotlin.attendmyparty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //this is one of lifecycle, first render
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        member_name.text = "..."
     }
 }
