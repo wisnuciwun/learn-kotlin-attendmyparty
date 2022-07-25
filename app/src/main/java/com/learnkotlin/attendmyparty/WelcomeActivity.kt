71b79ccc6bde2518f37d3d21b13caa5cac7d3cc9
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-25 11:22:01 +0700

    learn part 5 - add menu page & toggle button

--- app/src/main/java/com/learnkotlin/attendmyparty/WelcomeActivity.kt
+++ app/src/main/java/com/learnkotlin/attendmyparty/WelcomeActivity.kt
@@ -3,18 +3,17 @@ package com.learnkotlin.attendmyparty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_welcome.*
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.random.Random
 import kotlin.system.exitProcess
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     var memberName:String = ""
     val partyMate = arrayListOf<String>("Jeremy", "Dave", "Marvin", "Lexy", "Cindy")
     override fun onCreate(savedInstanceState: Bundle?) { //this is one of lifecycle, first render
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
         member_name.text = "..."
         main_menu.bringToFront()
 
@@ -39,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btn_close.setOnClickListener{
-            this@MainActivity.finish()
+            this@WelcomeActivity.finish()
             exitProcess(0)
         }
     }
