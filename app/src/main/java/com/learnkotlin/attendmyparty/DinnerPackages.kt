62cdf47179141100e6976553046b2af0c7296bca
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-26 10:46:25 +0700

    learn part 8 - dinner package page with onclick by function

---
+++ app/src/main/java/com/learnkotlin/attendmyparty/DinnerPackages.kt
@@ -0,0 +1,11 @@
+package com.learnkotlin.attendmyparty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DinnerPackages : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dinner_packages)
+    }
+}
--- app/src/main/java/com/learnkotlin/attendmyparty/MenuActivity.kt
+++ app/src/main/java/com/learnkotlin/attendmyparty/MenuActivity.kt
@@ -1,11 +1,20 @@
 package com.learnkotlin.attendmyparty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuActivity : LifecycleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun onHandleChangePage(view: View){
+        // view: View is important to show next page
+
+        val dinnerActivity = Intent(this, DinnerPackages::class.java)
+        startActivity(dinnerActivity)
     }
 }
